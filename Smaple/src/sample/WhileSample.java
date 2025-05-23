f7c0a06df5c96a8e9f1e0799aee755e01a8322d5
ShinnosukeEhara(江原慎之 <shinnosuke.ehara.y982@dirbato.co.jp>
2025-05-23 15:51:19 +0900

    commit5

---
+++ Smaple/src/sample/WhileSample.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class WhileSample {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+            var num = 2;
+
+            while (num <= 1024) {
+                System.out.println(num);
+                num = num * 2;
+        
+        /*var num = 2; 
+        while(num <= 1024) { 
+        System.out.println(num);*/
+            }
+    }
+    
+}
