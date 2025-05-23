ce4183c6bc92020517cf6a6b109e1eb24ebe474a
ShinnosukeEhara(江原慎之 <shinnosuke.ehara.y982@dirbato.co.jp>
2025-05-23 14:20:52 +0900

    commit3

---
+++ Smaple/src/sample/compare.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class compare {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        var a = 3;
+        var b = 2;
+        System.out.println("a > b は" + (a > b));
+        System.out.println("a < b は" + (a < b));
+    }
+
+}
